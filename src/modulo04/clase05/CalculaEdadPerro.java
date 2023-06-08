8004d8067a6d9b90e7a00f6384fa8fceb15963c6
HernanGoldy <hernanmdq@gmail.com>
2023-06-08 12:19:36 -0300

    modifico estructura de carpetas del bootcamp

--- src/modulo04/clase05/CalculaEdadPerro.java
+++ src/modulo04/clase05/CalculaEdadPerro.java
@@ -1,6 +1,12 @@
-package clase05;
+package modulo04.clase05;
 
 import java.util.Scanner;
+
+/*
+TODO: Actividad 1 - Calculadora de edad de perros
+* Pídele al usuario que ingrese la edad de su perro.
+Convierte esa edad a años de perro (se dice que 1 año humano equivale a 7 años de perro).
+ */
 
 public class CalculaEdadPerro {
 
@@ -18,6 +24,4 @@ public class CalculaEdadPerro {
 
         System.out.println("Fin del programa");
     }
-
-
 }
