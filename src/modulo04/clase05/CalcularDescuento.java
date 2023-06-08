211ca287d31c34c6769ac82e0e46ce19a1eb3a7b
HernanGoldy <hernanmdq@gmail.com>
2023-06-08 13:32:57 -0300

    agrego actividades de la clase03

---
+++ src/modulo04/clase05/CalcularDescuento.java
@@ -0,0 +1,17 @@
+package modulo04.clase05;
+
+/*
+TODO: Actividad 3 - Calculadora de descuento
+    * Pídele al usuario que ingrese el precio original de un producto.
+    * Pídele al usuario que ingrese el porcentaje de descuento.
+    * Calcula el precio final después de aplicar el descuento utilizando la fórmula: precioFinal = precioOriginal - (precioOriginal * descuento / 100).
+    * Muestra el precio final al usuario.
+ */
+
+public class CalcularDescuento {
+
+    public static void main(String[] args) {
+
+
+    }
+}
