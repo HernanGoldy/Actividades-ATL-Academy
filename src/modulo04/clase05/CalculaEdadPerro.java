dda1dfc92b3b5996efc5679d434613a804ad5164
HernanGoldy <hernanmdq@gmail.com>
2023-06-08 12:25:15 -0300

    modifico TODO CalculaEdadPerro

--- src/modulo04/clase05/CalculaEdadPerro.java
+++ src/modulo04/clase05/CalculaEdadPerro.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 /*
 TODO: Actividad 1 - Calculadora de edad de perros
-* Pídele al usuario que ingrese la edad de su perro.
-Convierte esa edad a años de perro (se dice que 1 año humano equivale a 7 años de perro).
+    * Pídele al usuario que ingrese la edad de su perro.
+    * Convierte esa edad a años de perro (se dice que 1 año humano equivale a 7 años de perro).
  */
 
 public class CalculaEdadPerro {
