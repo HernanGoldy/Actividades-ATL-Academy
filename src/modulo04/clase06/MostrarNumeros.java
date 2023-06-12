8c3f2da698cb3d25978db1b985f8befab48dc006
HernanGoldy <hernanmdq@gmail.com>
2023-06-12 10:53:15 -0300

    agrego actividad 7 - clase06

---
+++ src/modulo04/clase06/MostrarNumeros.java
@@ -0,0 +1,20 @@
+package modulo04.clase06;
+
+/*
+TODO: Actividad 7 - Muestra los números del 1 al 100 utilizando un bucle while
+ */
+
+public class MostrarNumeros {
+
+    public static void main(String[] args) {
+
+        int contador = 0;
+
+        while (contador < 100) {
+
+            contador++;
+
+            System.out.println(contador);
+        }
+    }
+}
