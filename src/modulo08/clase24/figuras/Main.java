f3aea169cb546625556c9251a1939dae440ef9b9
HernanGoldy <hernanmdq@gmail.com>
2023-07-31 05:57:54 -0300

    modifico actividad modulo08 - clase24

--- src/modulo08/clase24/figuras/Main.java
+++ src/modulo08/clase24/figuras/Main.java
@@ -1,5 +1,11 @@
 package modulo08.clase24.figuras;
 
+/*
+Ejercicio 1:
+Crea una interfaz FiguraGeometrica con el método calcularArea().
+Implementa esta interfaz en las clases Circulo y Rectangulo, calculando el área de cada figura en sus respectivas implementaciones.
+*/
+
 public class Main {
 
     public static void main(String[] args) {
